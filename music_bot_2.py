53f8c8eced0e93259a9e8472276ee7cbfc0cd8fa
ASNTHEGREAT <65227820+ASNTHEGREAT@users.noreply.github.com>
2020-09-20 16:56:22 +0530

    Create music_bot_2.py

---
+++ music_bot_2.py
@@ -0,0 +1,22 @@
+import time
+from selenium import webdriver
+import pyautogui as pg 
+
+video = input('Enter the name of the video: ')
+
+
+def video_player(video):
+    driver = webdriver.Chrome('chromedriver.exe')
+    driver.maximize_window()
+    driver.get('https://www.youtube.com/')
+    pg.moveTo(857, 130, duration=1.25) 
+    time.sleep(2)
+    pg.click()
+    pg.typewrite(video, interval=0.25)
+    pg.press('enter')
+    time.sleep(2)
+    pg.moveTo(369, 283, duration=1.15)
+    pg.click()
+
+
+video_player(video)
